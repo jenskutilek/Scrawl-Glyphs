ce1ee1055ff10837e6aebad995eccb5ba9385c70
Jens Kutilek <webmail@kutilek.de>
2018-05-24 17:23:53 +0200

    Use NSImage instead of Python data

--- Scrawl.glyphsReporter/Contents/Resources/plugin.py
+++ Scrawl.glyphsReporter/Contents/Resources/plugin.py
@@ -1,58 +1,15 @@
 # encoding: utf-8
-from __future__ import division
+from __future__ import division, print_function
+
 import objc
 from GlyphsApp import *
 from GlyphsApp.plugins import *
 
-import imp
-try:
-	imp.find_module('vanilla')
-	can_display_ui = True
-except ImportError:
-	can_display_ui = False
-	print "Please install vanilla to enable UI dialogs for Scrawl. You can install vanilla through Glyphs > Preferences > Addons > Modules."
+
+from AppKit import NSClassFromString, NSGraphicsContext, NSImage, NSImageInterpolationNone, NSMakeRect
 
 
-plugin_id = "de.kutilek.Scrawl"
-
-
-
-
-def getScrawl(layer):
-	pixel_size = layer.userData["%s.unit" % plugin_id]
-	if pixel_size is None:
-		pixel_size = 1 # font units
-
-	data = layer.userData["%s.data" % plugin_id]
-	if data is None:
-		data = []
-	else:
-		data = [(int(x), int(y)) for x, y in data]
-
-	return pixel_size, data
-
-
-def setGrid(layer, x, y=None, grid_type=None):
-	if x is None:
-		x = 0
-	if x == 0:
-		deleteGrid(layer)
-		return
-	if y is None:
-		y = x
-	layer.userData["%s.value" % plugin_id] = [x, y]
-	if grid_type is None:
-		if layer.userData["%s.type" % plugin_id] is not None:
-			del layer.userData["%s.type" % plugin_id]
-	else:
-		layer.userData["%s.type" % plugin_id] = grid_type
-
-
-def deleteScrawl(layer):
-	for key in ("unit", "data", "size"):
-		full_key = "%s.%s" % (plugin_id, key)
-		if layer.userData[full_key] is not None:
-			del layer.userData[full_key]
+plugin_id = "de.kutilek.scrawl"
 
 
 
@@ -61,34 +18,18 @@ class ScrawlReporter(ReporterPlugin):
 
 	def settings(self):
 		self.menuName = Glyphs.localize({'en': u'Scrawl'})
-
-
-	#def start(self):
-	#	if can_display_ui:
-	#		mainMenu = NSApplication.sharedApplication().mainMenu()
-	#		s = objc.selector(self.editMasterGrid,signature='v@:')
-	#		newMenuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
-	#			Glyphs.localize({
-	#				'en': u"Master Grid…",
-	#				'de': u'Master-Raster…'
-	#			}),
-	#			s,
-	#			""
-	#		)
-	#		newMenuItem.setTarget_(self)
-	#		submenu = mainMenu.itemAtIndex_(2).submenu()
-	#		submenu.insertItem_atIndex_(newMenuItem, submenu.numberOfItems())
-
 		
 	def background(self, layer):
 
 		# Check if the drawing should be shown
-		
+
 		currentController = self.controller.view().window().windowController()
 		if currentController:
 			tool = currentController.toolDrawDelegate()
-			if tool.isKindOfClass_(NSClassFromString("GlyphsToolText")) or tool.isKindOfClass_(NSClassFromString("GlyphsToolHand")):
+			if tool.isKindOfClass_(NSClassFromString("GlyphsToolText")) or tool.isKindOfClass_(NSClassFromString("GlyphsToolHand")) or tool.isKindOfClass_(NSClassFromString("ScrawlTool")):
 				return
+		
+		# find master for image positioning (descender)
 		
 		try:
 			master = layer.parent.parent.masters[layer.layerId]
@@ -96,16 +37,20 @@ class ScrawlReporter(ReporterPlugin):
 			return
 		if master is None:
 			return
-
-		pixel_size, data = getScrawl(layer)
-
-
-		NSColor.lightGrayColor().set()
-		#NSBezierPath.setDefaultLineWidth_(0.6/self.getScale())
-		for x, y in data:
-			NSBezierPath.fillRect_(NSMakeRect(x * pixel_size, y * pixel_size, pixel_size, pixel_size))
 		
+		# draw pixels
 
-
-	#def editMasterGrid(self):
-	#	GridDialog()
+		data = layer.userData["%s.data" % plugin_id]
+		if data is None:
+			return
+		try:
+			data = NSImage.alloc().initWithData_(data)
+		except:
+			return
+		font = layer.parent.parent
+		pad = int(round(font.upm / 10))
+		rect = NSMakeRect(-pad, master.descender - pad, 2 * pad + layer.width, 2 * pad + font.upm)
+		NSGraphicsContext.saveGraphicsState()
+		NSGraphicsContext.currentContext().setImageInterpolation_(NSImageInterpolationNone)
+		data.drawInRect_(rect)
+		NSGraphicsContext.restoreGraphicsState()
--- Scrawl.glyphsTool/Contents/Resources/plugin.py
+++ Scrawl.glyphsTool/Contents/Resources/plugin.py
@@ -6,12 +6,27 @@ from math import floor
 from GlyphsApp import GSOFFCURVE, GSQCURVE, GSCURVE
 from GlyphsApp.plugins import *
 
-from AppKit import NSShiftKeyMask, NSString # NSEventModifierFlagShift
+from AppKit import NSBezierPath, NSClassFromString, NSColor, NSData, NSGraphicsContext, NSImage, NSImageInterpolationNone, NSMakeRect, NSRoundLineCapStyle
 
 
-plugin_id = "de.kutilek.Scrawl"
+plugin_id = "de.kutilek.scrawl"
 default_pen_size = 1
-default_pixel_size = 8
+default_pixel_size = 10
+
+
+def initImage(layer, pixel_size=default_pixel_size):
+	upm = layer.parent.parent.upm
+	pad = int(round(upm / 10))
+	w = int(round((2 * pad + layer.width) / pixel_size))
+	h = int(round((2 * pad + upm) / pixel_size))
+	img = NSImage.alloc().initWithSize_((w, h))
+	# DEBUG: Draw a red rectangle around the image
+	#img.lockFocus()
+	#NSColor.redColor().set()
+	#NSBezierPath.setLineWidth_(1)
+	#NSBezierPath.strokeRect_(NSMakeRect(0, 0, w, h))
+	#img.unlockFocus()
+	return img
 
 
 def getScrawl(layer):
@@ -25,20 +40,25 @@ def getScrawl(layer):
 
 	data = layer.userData["%s.data" % plugin_id]
 	if data is None:
-		data = set()
+		data = initImage(layer, default_pixel_size)
 	else:
 		try:
-			data = set([(int(x), int(y)) for x, y in data])
+			data = NSImage.alloc().initWithData_(data)
 		except:
-			data = set()
+			data = initImage(layer, default_pixel_size)
 
 	return pen_size, pixel_size, data
 
 
-def setScrawl(layer, pen_size, pixel_size, data):
+def setScrawl(layer, pen_size, pixel_size, data=None):
 	layer.userData["%s.size" % plugin_id] = int(round(pen_size))
 	layer.userData["%s.unit" % plugin_id] = int(round(pixel_size))
-	layer.userData["%s.data" % plugin_id] = list(data)
+	if data is None:
+		del layer.userData["%s.data" % plugin_id]
+	else:
+		tiff = data.TIFFRepresentation()
+		print("Saving %i bytes ..." % len(tiff))
+		layer.userData["%s.data" % plugin_id] = tiff
 
 
 def deleteScrawl(layer):
@@ -56,23 +76,19 @@ class ScrawlTool(SelectTool):
 		from vanilla import ComboBox, Group, Slider, TextBox, Window 
 		self.name = 'Scrawl'
 		self.slider_value = 1 # current slider value
-		#self.coordinates = GlyphCoordinates()
-		#self.endPts = []
-		self.axis_tag = "" # current axis tag
-		self._axis_tags = []
 
 		# Create Vanilla window and group with controls
 		viewWidth = 180
-		viewHeight = 68
+		viewHeight = 42
 		self.sliderMenuView = Window((viewWidth, viewHeight))
 		self.sliderMenuView.group = Group((0, 0, viewWidth, viewHeight))
 		self.w = self.sliderMenuView.group
 		y = 0
-		self.w.text = TextBox((20, y, -20, 17), self.name)
+		self.w.text = TextBox((20, y, -20, 17), "%s Pen Size" % self.name)
 		y += 18
 		self.w.pen_size = Slider((20, y, -20, 24),
 			minValue = 1,
-			maxValue = 10,
+			maxValue = 100,
 			value = float(self.slider_value),
 			tickMarkCount = 0,
 			#stopOnTickMarks = False,
@@ -81,14 +97,13 @@ class ScrawlTool(SelectTool):
 			sizeStyle = 'small',
 		)
 
-
 		self.generalContextMenus = [
 			{"view": self.sliderMenuView.group.getNSView()}
 		]
 		self.keyboardShortcut = 'c'
 		self.pen_size = default_pen_size
 		self.pixel_size = default_pixel_size
-		self.data = []
+		self.data = None
 		self.prev_location = None
 		self.erase = False
 
@@ -114,9 +129,21 @@ class ScrawlTool(SelectTool):
 
 	def background(self, layer):
 		# draw pixels
-		NSColor.grayColor().set()
-		for x, y in self.data:
-			NSBezierPath.fillRect_(NSMakeRect(x * self.pixel_size, y * self.pixel_size, self.pixel_size, self.pixel_size))
+		if self.data is None:
+			return
+		font = layer.parent.parent
+		pad = int(round(font.upm / 10))
+		try:
+			master = layer.parent.parent.masters[layer.layerId]
+		except KeyError:
+			return
+		if master is None:
+			return
+		rect = NSMakeRect(-pad, master.descender - pad, 2 * pad + layer.width, 2 * pad + font.upm)
+		NSGraphicsContext.saveGraphicsState()
+		NSGraphicsContext.currentContext().setImageInterpolation_(NSImageInterpolationNone)
+		self.data.drawInRect_(rect)
+		NSGraphicsContext.restoreGraphicsState()
 
 	def keyDown_(self, event):
 		# Toggle between draw and eraser mode
@@ -125,25 +152,58 @@ class ScrawlTool(SelectTool):
 		else:
 			objc.super(ScrawlTool, self).keyDown_(event)
 
-	def setPixel(self, event):
+	def setPixel(self, event, dragging=False):
+		if self.data is None:
+			return
 		try:
 			editView = self.editViewController().graphicView()
 		except:
 			return
 		
-		Loc = editView.getActiveLocation_(event)
-		#layer = editView.activeLayer()
-		#font = layer.font()
+		layer = editView.activeLayer()
+		try:
+			master = layer.parent.parent.masters[layer.layerId]
+		except KeyError:
+			return
+		if master is None:
+			return
 		
-		loc_pixel = set([(
-			int(floor(Loc.x / self.pixel_size)),
-			int(floor(Loc.y / self.pixel_size))
-		)])
+		Loc = editView.getActiveLocation_(event)
+		pad = int(round(layer.parent.parent.upm / 10))
+		loc_pixel = (
+			int(round((Loc.x + pad) / self.pixel_size)),
+			int(round((Loc.y + pad - master.descender) / self.pixel_size))
+		)
 		if self.prev_location is None or self.prev_location != loc_pixel:
+			#print(loc_pixel)
+			x, y = loc_pixel
+			self.data.lockFocus()
 			if self.erase:
-				self.data = self.data - loc_pixel
+				NSColor.whiteColor().set()
 			else:
-				self.data = self.data | loc_pixel
+				NSColor.darkGrayColor().set()
+			if dragging and self.prev_location is not None:
+				px, py = self.prev_location
+				path = NSBezierPath.alloc().init()
+				NSBezierPath.setLineCapStyle_(NSRoundLineCapStyle)
+				NSBezierPath.setLineWidth_(self.pen_size)
+				#path.strokeLineFromPoint_toPoint_(x, y, x + self.pen_size, y + self.pen_size)
+				path.moveToPoint_((px, py))
+				path.lineToPoint_((x, y))
+				path.stroke()
+			else:
+				half = self.pen_size / 2
+				rect = NSMakeRect(
+					x - half,
+					y - half,
+					self.pen_size,
+					self.pen_size
+				)
+				path = NSBezierPath.bezierPathWithOvalInRect_(rect)
+				path.fill()
+			# For rectangular pens:
+			#NSBezierPath.fillRect_(rect)
+			self.data.unlockFocus()
 			self.prev_location = loc_pixel
 
 	def mouseDown_(self, event):
@@ -157,7 +217,7 @@ class ScrawlTool(SelectTool):
 		self.updateView()
 	
 	def mouseDragged_(self, event):
-		self.setPixel(event)
+		self.setPixel(event, True)
 		self.updateView()
 		
 	def mouseUp_(self, event):
@@ -175,5 +235,5 @@ class ScrawlTool(SelectTool):
 
 	def slider_callback(self, sender=None):
 		if sender is not None:
-			self.pen_size = "%i" % sender.get()
+			self.pen_size = int("%i" % sender.get())
 			self.updateView()
