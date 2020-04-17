8631895a04fe9f0173dad7bf2c6ab6e15d822f66
Jens Kutilek <webmail@kutilek.de>
2020-04-17 18:02:35 +0200

    Indentation to spaces

--- scripts/Make Random Data.py
+++ scripts/Make Random Data.py
@@ -5,17 +5,17 @@ from random import randint
 pid = "de.kutilek.Scrawl"
 s = 5
 data = [
-	(
-		randint(
-			int(round(-100 / s)),
-			int(round((Layer.width + 100) / s)),
-		),
-		randint(
-			int(round(Layer.parent.parent.masters[Layer.parent.parent.selectedFontMaster.id].descender / s)),
-			int(round(Layer.parent.parent.masters[Layer.parent.parent.selectedFontMaster.id].ascender / s)),
-		),
-	)
-	for i in range(2000)
+    (
+        randint(
+            int(round(-100 / s)),
+            int(round((Layer.width + 100) / s)),
+        ),
+        randint(
+            int(round(Layer.parent.parent.masters[Layer.parent.parent.selectedFontMaster.id].descender / s)),
+            int(round(Layer.parent.parent.masters[Layer.parent.parent.selectedFontMaster.id].ascender / s)),
+        ),
+    )
+    for i in range(2000)
 ]
 Layer.userData["%s.unit" % pid] = s
 Layer.userData["%s.data" % pid] = data
